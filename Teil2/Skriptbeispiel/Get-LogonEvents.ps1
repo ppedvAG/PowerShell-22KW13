13ac9a902f8688d363044248f3cc410aec051d16
Stefan Ober <StefanO@ppedv.de>
2022-03-30 13:32:55 +0200

    Skript mit Parametern

---
+++ Teil2/Skriptbeispiel/Get-LogonEvents.ps1
@@ -0,0 +1,14 @@
+﻿[cmdletBinding()]
+param(
+[Parameter(Mandatory=$true)]
+[int]$EventId,
+
+[int]$Newest = 5,
+
+[string]$Computername = "localhost"
+)
+
+Write-Verbose -Message "Das Skript wurde mit folgenden Werten gestartet: $EventId | $Newest | $Computername"
+
+Get-EventLog -LogName Security -ComputerName $Computername | Where-Object EventId -eq $EventId | Select-Object -First $Newest
+
